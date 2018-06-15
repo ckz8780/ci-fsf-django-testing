79544a8d8723e0edb6330f9d95f2fea69b2f8713
Chris Zielinski <ckz8780@gmail.com>
2018-06-15 21:49:44 +0000

    Added more view tests - now 100% coverage

--- todo/test_views.py
+++ todo/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.shortcuts import get_object_or_404
 from .models import Item
 
 # Create your tests here.
@@ -28,3 +29,28 @@ class TestViews(TestCase):
         # This time all we do is send through a known bad item ID (or bacon, because bacon is delicious)
         page = self.client.get('/edit/{}/'.format('bacon'))
         self.assertEqual(page.status_code, 404)
+        
+    def test_post_create_an_item(self):
+        response = self.client.post("/add/", {"name": "Create a Test"})
+        item = get_object_or_404(Item, pk=1)
+        self.assertEqual(item.done, False)
+    
+    def test_post_edit_an_item(self):
+        item = Item(name="Create a Test")
+        item.save()
+        item_id = item.id
+
+        response = self.client.post("/edit/{}/".format(item_id), {"name": "A different name"})
+        item = get_object_or_404(Item, pk=item_id)
+
+        self.assertEqual("A different name", item.name)
+    
+    def test_toggle_status(self):
+        item = Item(name="Create a Test")
+        item.save()
+        item_id = item.id
+
+        response = self.client.post("/toggle/{}/".format(item_id))
+
+        item = get_object_or_404(Item, pk=item_id)
+        self.assertEqual(item.done, True)
