f9589296db1ef6654914bd0f1cdba79baa1c7493
Chris Zielinski <ckz8780@gmail.com>
2018-06-15 21:38:51 +0000

    added new tests for models - now 100% coverage

--- todo/test_models.py
+++ todo/test_models.py
@@ -20,3 +20,7 @@ class TestItemModel(TestCase):
         # Now test its props for correctness
         self.assertEqual(item.name, "Create a Test")
         self.assertTrue(item.done)
+        
+    def test_item_as_a_string(self):
+        item = Item(name='Create a Test')
+        self.assertEqual('Create a Test', str(item))
