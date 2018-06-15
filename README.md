7c70ce8f31e6f142d54d87b2692e0e6735c51edf
Chris Zielinski <ckz8780@gmail.com>
2018-06-15 21:27:32 +0000

    added model tests

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Welcome!
 
 This is a clone of the django_todo project, but extended to implement testing. It's part of the Full Stack Frameworks with Django module for CodeInstitute.
+
+Relevant tests are in: 
+
+- test_forms.py
+- test_views.py
+- test_models.py
---
+++ todo/test_models.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+from .models import Item
+
+class TestItemModel(TestCase):
+
+    def test_done_defaults_to_false(self):
+        # First create/ssave the item in the test DB
+        item = Item(name="Create a Test")
+        item.save()
+        
+        # Now let's check that we have the right item, and that its 'done' property is false
+        self.assertEqual(item.name, "Create a Test")
+        self.assertFalse(item.done)
+    
+    def test_can_create_an_item_with_a_name_and_status(self):
+        # First create/ssave the item in the test DB
+        item = Item(name="Create a Test", done=True)
+        item.save()
+        
+        # Now test its props for correctness
+        self.assertEqual(item.name, "Create a Test")
+        self.assertTrue(item.done)
