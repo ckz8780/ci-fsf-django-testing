dd2cd6e3a1313df415b34f06687e9112d6f0d3bd
Chris Zielinski <ckz8780@gmail.com>
2018-06-15 22:34:45 +0000

    installed postgres reqs and migrated to postgres

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -77,11 +78,15 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse('postgres://snvqbxxejblvrz:f522185e0ba7f89d186ac9067298e8c76edcd254ab155d7afddbf0c41dc58d40@ec2-54-235-252-23.compute-1.amazonaws.com:5432/d54ee790nsaoak')
 }
 
 
